7d92385b9c915d77c35c3b279eaa753ded35ad37
RobinSlimani <126561367+RobinSlimani@users.noreply.github.com>
2024-06-07 09:18:36 +0200

    Delete sauvegarde3 directory

--- sauvegarde3/php/src/js/utilities.js
+++
@@ -1,7 +0,0 @@
-function showErrorMessage(message) {
-    const errorMessageElement = document.getElementById('error-message');
-    errorMessageElement.textContent = message;
-    setTimeout(() => {
-        errorMessageElement.textContent = '';
-    }, 3000);
-}
